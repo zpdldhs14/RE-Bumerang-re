642bdd84984f12725a5e25a9ff8e44dcc44c2fd6
project1 <nagelgood123@gmail.com>
2024-03-23 00:45:11 +0900

    qwer

--- Assets/Scripts/UI/Fade/UIImage/AlphaZero.cs
+++ Assets/Scripts/UI/Fade/UIImage/AlphaZero.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class AlphaZero : MonoBehaviour
 {
@@ -9,5 +10,14 @@ public class AlphaZero : MonoBehaviour
     void Start()
     {
         UIManager.Instance.FadeAlphaZero(this.gameObject, duration);
+    }
+
+    void Update()
+    {
+        Image image = gameObject.GetComponent<Image>();
+        if(image.color.a == 0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
